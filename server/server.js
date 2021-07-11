09703cbcc574efd9b601a10b03bd46285a6f4c1a
Sqwyer <sawyer.codes.stuff@gmail.com>
2021-07-11 11:45:45 -0500

    Added build support in server

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const path = require('path');
 const express = require("express");
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
